22586b3df186638d8a816c677c716c33c98f64f4
lucas <l.bielecki13@gmail.com>
2021-08-18 21:28:43 +0200

    active link switching

--- js/main.js
+++ js/main.js
@@ -41,7 +41,7 @@ $(document).ready(function(){
 
 	// sticky navbar animation
 
-	function navbarFixed(){
+	function navbarAnimation(){
 		let nav_offset_top = $('.header_area').height() + 50;
 		if($('.header_area').length){
 			$(window).scroll(function(){
@@ -55,37 +55,38 @@ $(document).ready(function(){
 		};
 	};
 
-	navbarFixed();
+	navbarAnimation();
+
+	// smooth scroll
+
+	let scrollLink = $('.scroll');
+
+	scrollLink.click(function(event) {
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: $(this.hash).offset().top - 80
+		}, 1000 )
+	});
+
+	// menu active link switching
+
+	$(window).scroll(function() {
+		let scrollbarLocation = $(this).scrollTop();
+
+		scrollLink.each(function() {
+
+			let sectionOffset = $(this.hash).offset().top;
+
+			if (sectionOffset <= scrollbarLocation + 85) {
+				$(this).parent().addClass('active');
+				$(this).parent().siblings().removeClass('active');
+			}
+
+		})
+
+	})
 
 });
-
-// smooth scroll
-
-// Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top - 80
-        }, 1000)
-      }
-    }
-  });
 
 
 
